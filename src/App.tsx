818906331a9389a35984456afb3f761621cfaa10
JeongbinYoon <yon980824@gmail.com>
2022-05-25 16:05:53 +0900

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,158 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './reset.css';
 
+function HdCoffee(){
+  console.log('hdcoffee');
+  return(
+    <div className='HdCoffee'>
+      <ul>
+        <li>
+          <ul className='item'>
+            <li><a href="#0"><h2>커피</h2></a></li>
+            <li><a href="#0">스타벅스 원두</a></li>
+            <li><a href="#0">스타벅스 원두</a></li>
+            <li><a href="#0">스타벅스 원두</a></li>
+          </ul>
+        </li>
+
+        <li>
+          <ul className='item'>
+            <li><h2>나와 어울리는 커피</h2></li>
+            <li>스타벅스 원두</li>
+            <li>스타벅스 원두</li>
+            <li>스타벅스 원두</li>
+          </ul>
+        </li>
+
+        <li>
+          <ul className='item'>
+            <li><h2>커피333</h2></li>
+            <li>스타벅스 원두</li>
+            <li>스타벅스 원두</li>
+            <li>스타벅스 원두</li>
+          </ul>
+        </li>
+
+        <li>
+          <ul className='item'>
+            <li><h2>커피444</h2></li>
+            <li>스타벅스 원두</li>
+            <li>스타벅스 원두</li>
+            <li>스타벅스 원두</li>
+          </ul>
+        </li>
+
+        <li>
+          <ul className='item'>
+            <li><h2>커피555</h2></li>
+            <li>스타벅스 원두</li>
+            <li>스타벅스 원두</li>
+            <li>스타벅스 원두</li>
+          </ul>
+        </li>
+
+        <li>
+          <ul className='item'>
+            <li><h2>커피666</h2></li>
+            <li>스타벅스 원두</li>
+            <li>스타벅스 원두</li>
+            <li>스타벅스 원두</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function HdMenu(){
+  console.log('hdmenu');
+  return(
+    <div className='HdMenu'>
+
+    </div>
+  );
+}
+
+function HdStore(){
+  return(
+    <div className='HdStore'>
+
+    </div>
+  );
+}
+
+function HdResponsibility(){
+  return(
+    <div className='HdResponsibility'>
+
+    </div>
+  );
+}
+
+function HdStarbuckRewards(){
+  return(
+    <div  className='HdStarbuckRewards'>
+
+    </div>
+  );
+}
+
+function HdWhatsNew(){
+  return(
+    <div  className='HdWhatsNew'>
+
+    </div>
+  );
+}
+
 function Header(){
+  const [selected, setSelected] = useState(-1);
   return (
     <header>
-      <img src="./header.png" alt="header" />
+
+      <div className='header' onMouseLeave={()=>{
+        setSelected(-1);
+      }}>
+        <img src="./header.png" alt="header" />
+        <div className='nav'>
+          <ul className="header_ul">
+            <li onMouseOver={()=>{
+              console.log('coffee click');
+              setSelected(0);
+            }}>COFFEE</li>
+            <li onMouseOver={()=>{
+              console.log('menu click');
+              setSelected(1);
+            }}>MENU</li>
+            <li onMouseOver={()=>{
+              console.log('store click');
+              setSelected(2);
+            }}>STORE</li>
+            <li onMouseOver={()=>{
+              console.log('responsibility click');
+              setSelected(3);
+            }}>RESPONSIBILITY</li>
+            <li onMouseOver={()=>{
+              console.log('starbucks rewards click');
+              setSelected(4);
+            }}>STARBUCKS REWARDS</li>
+            <li onMouseOver={()=>{
+              console.log('whats new click');
+              setSelected(5);
+            }}>WHAT'S NEW</li>
+          </ul>
+        </div>
+
+        <div className='subNav'>
+        {(selected === 0) && <HdCoffee />}
+          {(selected === 1) && <HdMenu />}
+          {(selected === 2) && <HdStore />}
+          {(selected === 3) && <HdResponsibility />}
+          {(selected === 4) && <HdStarbuckRewards />}
+          {(selected === 5) && <HdWhatsNew />}
+        </div>
+      </div>
     </header>
   );
 }
@@ -13,7 +160,9 @@ function Header(){
 function Main(){
   return(
     <section>
-      <img src="./main.png" alt="main" />
+      <div className='Main'>
+        <img src="./main.png" alt="main" />
+      </div>
     </section>
   )
 }
